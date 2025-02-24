13d46702feea09d69ffb0cdf18e4cfc52e317281
FelipePavanelli <felipe.chamarelli@hotmail.com>
2025-02-24 12:54:51 -0300

    Update Server teste

--- chat/my-app/src/App.js
+++ chat/my-app/src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import ChatApp from "./ChatApp";
+import ChatApp from "./ChatApp.js";
 
 function App() {
   const { isLoading } = useAuth0();
