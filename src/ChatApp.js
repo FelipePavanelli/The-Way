933ae0ead788bfce141f40191f9c1069b36f60fa
FelipePavanelli <felipe.chamarelli@hotmail.com>
2025-02-26 18:32:09 -0300

    New features

--- src/ChatApp.js
+++ src/ChatApp.js
@@ -227,9 +227,9 @@ function ChatApp() {
 
     try {
       // Modo local
-      const response = await fetch("http://localhost:4000/api/agent", {
+      // const response = await fetch("http://localhost:4000/api/agent", {
         // produção:
-        // const response = await fetch("https://api.theway.altavistainvest.com.br/api/agent", {
+        const response = await fetch("https://api.theway.altavistainvest.com.br/api/agent", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
