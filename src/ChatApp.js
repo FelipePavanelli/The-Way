2aa2abccc2664087dc03ac7f0df4c60c19499e5e
FelipePavanelli <felipe.chamarelli@hotmail.com>
2025-03-10 18:45:54 -0300

    s

--- src/ChatApp.js
+++ src/ChatApp.js
@@ -41,10 +41,10 @@ function getUserInitial(user) {
 
 function getInitialMessage(user) {
   if (user) {
-    const userName = user.name || user.nickname || user.email;
-    return `Olá, ${userName}! No que posso te ajudar hoje?`;
+    // const userName = user.name || user.nickname || user.email;
+    return `Como posso te ajudar hoje?`;
   }
-  return "Olá, no que posso te ajudar hoje?";
+  return "Como posso te ajudar hoje?";
 }
 
 function ChatApp() {
@@ -375,7 +375,7 @@ function ChatApp() {
     setShowThinking(true);
 
     try {
-      const response = await fetch("https://api.theway.altavistainvest.com.br/api/agent", {
+      const response = await fetch("http://localhost:4000/api/agent", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
--- src/Server.js
+++ src/Server.js
@@ -39,7 +39,7 @@ app.post("/api/agent", async (req, res) => {
 
   try {
     const flowResp = await fetch(
-      "https://n8n.altavistainvest.com.br/webhook/27a5a92e-e71e-45c1-aecd-0c36d112b94c",
+      "https://n8n.altavistainvest.com.br/webhook-test/27a5a92e-e71e-45c1-aecd-0c36d112b94c",
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -90,7 +90,7 @@ app.post("/api/sendEmail", async (req, res) => {
 
   try {
     const flowResp = await fetch(
-      "https://n8n.altavistainvest.com.br/webhook/27a5a92e-e71e-45c1-aecd-0c36d112b94c",
+      "https://n8n.altavistainvest.com.br/webhook-test/27a5a92e-e71e-45c1-aecd-0c36d112b94c",
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
