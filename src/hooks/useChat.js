5b076f0690fd17af070de405d340934ff13156ef
FelipePavanelli <felipe.chamarelli@hotmail.com>
2025-05-09 11:35:25 -0300

    V1.3.0

--- src/hooks/useChat.js
+++ src/hooks/useChat.js
@@ -153,7 +153,6 @@ export function useChat(user) {
 
     try {
       // Escolher o endpoint baseado no modo selecionado
-      // const endpoint = useAV ? "http://localhost:4000/api/thewayiaav" : "http://localhost:4000/api/agent";
       const endpoint = useAV ? "https://api.theway.altavistainvest.com.br/api/thewayiaav" : "https://api.theway.altavistainvest.com.br/api/agent";
       console.log(`Enviando requisição para ${endpoint} (modo AV: ${useAV ? "ativado" : "desativado"})`);
       
@@ -226,7 +225,6 @@ export function useChat(user) {
     setTimeout(async () => {
       try {
         // Fazer a requisição para o servidor
-        // const endpoint = "http://localhost:4000/api/agent";
         const endpoint = "https://api.theway.altavistainvest.com.br/api/agent";
         const response = await fetch(endpoint, {
           method: "POST",
